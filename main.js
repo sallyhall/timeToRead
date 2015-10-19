88c71c144f62fada80d01d9ea4530226dcbbfe10
Sally Kingston <sally.a.kingston@gmail.com>
2015-10-19 14:38:49 -0400

    fixed hard mode

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-var wordsPerMinute = 230;
 
-$.fn.getTimeToRead = function () {
+$.fn.getTimeToRead = function (wpm) {
+  if (typeof wpm != "number"){
+    wpm=230;
+  }
   var paragraphTextArray = [].slice.call(this.find('p'));
   var totalText = "";
   paragraphTextArray.forEach(function(paragraph) {
     totalText += paragraph.textContent;
   });
   var wordCount = totalText.split(" ").length;
-  var totalTime = Math.round(wordCount/wordsPerMinute);
+  var totalTime = Math.round(wordCount/wpm);
   return totalTime;
 }
 
